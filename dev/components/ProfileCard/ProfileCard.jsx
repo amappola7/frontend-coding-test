727b8ea751784488161dff74f716a451c697090d
Amappola <anam.porrasp@gmail.com>
2022-12-08 20:26:26 -0500

    Setting up files for ProfileCard and TaskCard components

---
+++ dev/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './ProfileCard.module.scss'
+
+function ProfileCard() {
+  return (
+    <div className={`${styles.profileCard}`}>ProfileCard</div>
+  )
+}
+
+export default ProfileCard
---
+++ dev/components/TaskCard/TaskCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './TaskCard.module.scss'
+
+function TaskCard() {
+  return (
+    <div className={`${styles.taskCard}`}>TaskCard</div>
+  )
+}
+
+export default TaskCard
