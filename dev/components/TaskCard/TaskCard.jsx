d1a2d810f0a100ad6e2db9f63310ef9cd47265d2
Amappola <anam.porrasp@gmail.com>
2022-12-08 22:23:32 -0500

    Adding head tag, favicon and CDN for fontawesome icons

--- dev/components/TaskCard/TaskCard.jsx
+++ dev/components/TaskCard/TaskCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import styles from './TaskCard.module.scss'
 
-function TaskCard() {
+function TaskCard({taskTitle, taskEndDate, taskDescription}) {
+  const completed = false;
   return (
-    <div className={`${styles.taskCard}`}>TaskCard</div>
+    <div className={`${styles.taskCard}`}>
+      <span className={styles.taskCard__taskButtons}>
+        <button className={styles.taskCard__completeTaskButton}>{completed === false ? <i class="fa-regular fa-square-check"></i> : <i class="fa-solid fa-square-check"></i>}</button>
+        <button className={styles.taskCard__editTaskButton}>Edit</button>
+      </span>
+      <span className={styles.taskCard__taskInfo}>
+        <p className={styles.taskCard__taskTitle}>{taskTitle}</p>
+        <p className={styles.taskCard__taskEndDate}>{`End date: ${taskEndDate}`}</p>
+        <p className={styles.taskCard__taskDescription}>{taskDescription}</p>
+      </span>
+    </div>
   )
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,43 @@
-import ProfileCard from "../dev/components/ProfileCard/ProfileCard";
+import Head from 'next/head'
+import TaskCard from "../dev/components/TaskCard/TaskCard";
 import GenericBox from "../dev/components/GenericBox/GenericBox";
 import globalStyles from '../styles/globals.module.scss'
 
 function Index() {
   return (
-  <div>
-    <style jsx global>
-      {`
-        html {
-          font-family: 'IBM Plex Sans', sans-serif;
-          font-size: 62.5%;
-          color: #202124;
-        }
+    <>
+    <Head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <link rel="apple-touch-icon" sizes="180x180" href="../public/favicon_io/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon_io/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon_io/favicon-16x16.png" />
+      <link rel="manifest" href="../public/favicon_io/site.webmanifest" />
+      <title>to_dosketch</title>
+    </Head>
+    <div>
+      <style jsx global>
+        {`
+          html {
+            font-family: 'IBM Plex Sans', sans-serif;
+            font-size: 62.5%;
+            color: #202124;
+          }
 
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-        }
-      `}
-    </style>
-      <GenericBox>
-        <ProfileCard userPicture='https://image.shutterstock.com/mosaic_250/2780032/1714666150/stock-photo-head-shot-portrait-close-up-smiling-confident-businessman-wearing-glasses-looking-at-camera-1714666150.jpg' userName='Harvey Specter' userOccupation='Lawyer' userAge='20'  />
-      </GenericBox>
-  </div>
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+        `}
+      </style>
+        <GenericBox>
+          <TaskCard></TaskCard>
+        </GenericBox>
+    </div>
+    </>
   )
 }
 
